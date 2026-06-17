8fdd4367195bcb35327572761a95bbf1eab0d11b
hemeswarborah1-beep <hemeswarborah1@gmail.com>
2026-06-17 12:49:38 +0530

    Add files via upload

---
+++ gemini-code-1781678628233.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'tour-diary-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // We don't cache Firestore API calls, only the app shell (HTML/JS)
+  if (event.request.url.includes('firestore.googleapis.com')) {
+    return; 
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
